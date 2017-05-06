4a846f2443c316d62dd943f7cc3296c0a951a965
BenJule <BenJule@users.noreply.github.com>
2017-05-06 17:08:38 +0200

    Update xenonhd.mk

--- xenonhd.mk
+++ xenonhd.mk
@@ -2,7 +2,12 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from device
-$(call inherit-product, device/samsung/i9300/device.mk) -- path to main device makefile
+$(call inherit-product, device/samsung/smdk4412-common/common.mk)
+$(call inherit-product, device/samsung/i9300/i9300.mk)
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
 
 # Inherit common product files.
 $(call inherit-product, vendor/xenonhd/config/common_full_phone.mk)
@@ -10,10 +15,10 @@ $(call inherit-product, vendor/xenonhd/config/common_full_phone.mk)
 # Set those variables here to overwrite the inherited values.
 BOARD_VENDOR := 
 PRODUCT_BRAND := 
-PRODUCT_DEVICE := 
-PRODUCT_NAME := xenonhd_device
-PRODUCT_MANUFACTURER := 
-PRODUCT_MODEL := 
+PRODUCT_DEVICE := i9300
+PRODUCT_NAME := xenonhd_i9300
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_MODEL := GT-I9300
 TARGET_VENDOR := 
 
 # Root options
@@ -30,6 +35,8 @@ PRODUCT_PACKAGES += \
 # Use the latest approved GMS identifiers unless running a signed build
 ifneq ($(SIGN_BUILD),true)
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT=specific for your device \
-    PRIVATE_BUILD_DESC="specific for your device"
+		PRODUCT_NAME=m0xx \
+		TARGET_DEVICE=m0 \
+		BUILD_FINGERPRINT="samsung/m0xx/m0:4.3/JSS15J/I9300XXUGMJ9:user/release-keys" \
+		PRIVATE_BUILD_DESC="m0xx-user 4.3 JSS15J I9300XXUGMJ9 release-keys"
 endif
